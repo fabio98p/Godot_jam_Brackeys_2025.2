f515d3a7be61ca1faa30fad741dcf18a83727957
fabio parisi <parisifabio98@gmail.com>
2025-08-15 22:08:05 +0200

    add fmod plugin and banks for music

--- Menus/main_menu.gd
+++ Menus/main_menu.gd
@@ -1,11 +1,13 @@
 extends Control
 
+@onready var fmod_event_emitter_2d: FmodEventEmitter2D = $FmodEventEmitter2D
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
 
 func _on_option_pressed() -> void:
 	print("test")
+	fmod_event_emitter_2d.play()
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
