d59c7f1a9d48cb33a01577307a2b9011923e7c3e
fabio parisi <parisifabio98@gmail.com>
2025-08-24 11:59:53 +0200

    copy from template

--- Menus/main_menu.gd
+++ Menus/main_menu.gd
@@ -1,11 +1,10 @@
 extends Control
 
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
 
-func _on_option_pressed() -> void:
-	print("test")
+func _on_credits_pressed() -> void:
+	print("start credits")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
